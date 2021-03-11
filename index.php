971340e8413507c98720f9ea57563c7d1f9e3d1a
Nathaniel White <natewhite345@gmail.com>
2021-03-11 16:37:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php if (!array_key_exists('p', $_GET)) {
-    $contentstring="Put your text here yo";
+    $contentstring="Put your text here, yo";
     $pageinfo=["page"=>"home","title"=>"College Questions","content"=>$contentstring];
 }
 include "init.php";
