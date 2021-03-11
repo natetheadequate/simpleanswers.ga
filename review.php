ca5751d53586cb950ffb0a298a21cf85d1e743c2
Nathaniel White <natewhite345@gmail.com>
2021-03-11 17:20:58 -0500

    relative link for review.php

--- review.php
+++ review.php
@@ -9,7 +9,7 @@ $database=$DB;
 $datafile=$database->query("SELECT `title`,`p` FROM `Questions` WHERE (`comments` is null or (`comments` Not Like '%~Will%' And `comments` Not like '%~Nate%' And `comments` Not like '%-Nate%' And `comments` Not like '%-Will%')) and `p`>-1");
 if (isset($datafile)) {
     while ($data=$datafile->fetch_array()) {
-        echo '<big><a target="_blank" href="http://www.simpleanswers.dx.am?p='.$data['p'].'">"'.$data['title'].'"</a></big><br />';
+        echo '<big><a target="_blank" href="/?p='.$data['p'].'">"'.$data['title'].'"</a></big><br />';
     }
 } else {
     echo "No unanswered questions at the moment";
