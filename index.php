56473083281b355f5f2e1a4d4fafb90f600c9024
Nathaniel White <natewhite345@gmail.com>
2021-03-11 22:34:12 -0500

    Restore to original text

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php if (!array_key_exists('p', $_GET)) {
-    $contentstring="Put your text here yo";
-    $pageinfo=["page"=>"home","title"=>"College Questions","content"=>$contentstring];
+<?php
+if (!array_key_exists('p', $_GET)) {
+    $contentstring="&#9;Kids are curious about the world around them and have lots of questions. However, in many cases, the answers to their questions may seem like they can only be understood by older audiences, standing in the way of their curiosity. We believe that with explanations at the right level, without any assumed knowledge beyond basic arithmetic, kids can understand the answers to their questions and remain curious about the world around them.<br />&#9; The arrows to the right of the title of the question will increase or decrease its positioning on the sidebar.";
+    $pageinfo=["page"=>"home","title"=>"Welcome to Simple Answers!","content"=>$contentstring];
 }
-include "init.php";
+require "init.php";
